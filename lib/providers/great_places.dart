ebcde9706bd0b6ac95435473290b730d6f8d45fa
souzap371 <souzap371@gmail.com>
2022-10-15 09:14:01 -0300

    configurando API googleMap

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -7,7 +7,22 @@ import 'package:great_places/models/place.dart';
 import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
-  final List<Place> _items = [];
+  List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
